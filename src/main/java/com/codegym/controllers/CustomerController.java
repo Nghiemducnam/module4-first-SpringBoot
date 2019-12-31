e5c00641e50d335fe4769c656d0eef41400c0331
nghiemducnam <nghiemducnam0312@gmail.com>
2019-12-31 22:02:47 +0700

    edit a customer

--- src/main/java/com/codegym/controllers/CustomerController.java
+++ src/main/java/com/codegym/controllers/CustomerController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.util.UriComponentsBuilder;
 
 import java.util.List;
+import java.util.Optional;
 
 @CrossOrigin(maxAge = 3600)
 @RestController
@@ -33,5 +34,28 @@ public class CustomerController {
         HttpHeaders headers = new HttpHeaders();
         headers.setLocation(ucBuilder.path("/admin/customer/{id}").buildAndExpand(customer.getId()).toUri());
         return new ResponseEntity<Customer>(headers, HttpStatus.CREATED);
+    }
+
+    @RequestMapping(value = "/admin/customer/{id}", method = RequestMethod.GET)
+    ResponseEntity<?> getCustomer(@PathVariable ("id") Long id){
+        Optional <Customer> customer = customerService.findById(id);
+        if(!customer.isPresent()){
+            return new ResponseEntity<Customer>(HttpStatus.NOT_FOUND);
+        }
+        return new ResponseEntity<>(customer, HttpStatus.OK);
+    }
+
+    @RequestMapping(value = "/admin/customer/{id}", method = RequestMethod.PUT)
+    ResponseEntity<?> editCustomer(@PathVariable ("id") Long id, @RequestBody Customer customer){
+        Optional<Customer> currentCustomer = customerService.findById(id);
+        if(!currentCustomer.isPresent()){
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        currentCustomer.get().setFirstName(customer.getFirstName());
+        currentCustomer.get().setLastName(customer.getLastName());
+        currentCustomer.get().setId(customer.getId());
+        customerService.save(currentCustomer.get());
+
+        return new ResponseEntity <>( currentCustomer, HttpStatus.OK);
     }
 }
