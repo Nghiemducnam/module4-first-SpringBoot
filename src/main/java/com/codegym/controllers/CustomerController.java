aa92a8bd924f1c31e2e2a815fc6707d30ed4a07d
nghiemducnam <nghiemducnam0312@gmail.com>
2019-12-31 21:31:20 +0700

    create a new customer

--- src/main/java/com/codegym/controllers/CustomerController.java
+++ src/main/java/com/codegym/controllers/CustomerController.java
@@ -4,11 +4,11 @@ import com.codegym.models.Customer;
 import com.codegym.services.CustomerService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import java.util.List;
 
@@ -17,6 +17,7 @@ import java.util.List;
 public class CustomerController {
     @Autowired
     private CustomerService customerService;
+
     @RequestMapping(value = "/user/customer/")
     ResponseEntity<List<Customer>> findAllCustomer(){
         List<Customer> customers = (List<Customer>) customerService.findAll();
@@ -24,5 +25,13 @@ public class CustomerController {
             return new ResponseEntity<List<Customer>>(HttpStatus.NO_CONTENT);
         }
         return new ResponseEntity<List<Customer>>(customers, HttpStatus.OK);
+    }
+
+    @RequestMapping(value = "/admin/customer/", method = RequestMethod.POST)
+    ResponseEntity<Customer> createCustomer(@RequestBody Customer customer, UriComponentsBuilder ucBuilder){
+        customerService.save(customer);
+        HttpHeaders headers = new HttpHeaders();
+        headers.setLocation(ucBuilder.path("/admin/customer/{id}").buildAndExpand(customer.getId()).toUri());
+        return new ResponseEntity<Customer>(headers, HttpStatus.CREATED);
     }
 }
